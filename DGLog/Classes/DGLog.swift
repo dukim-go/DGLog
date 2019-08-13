b529840f503b3e87a1270979278478943fd9d543
daeung Kim <dwkim@gpmobile.co.kr>
2019-08-13 22:05:26 +0900

    log modify

--- DGLog/Classes/DGLog.swift
+++ DGLog/Classes/DGLog.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// DGLogConfig
 public struct DGLogConfig {
     public static var isVisible = true
     public static var isVisibleDate = true
@@ -9,27 +10,52 @@ public struct DGLogConfig {
     public static var isVisibleMessage = true
 }
 
+/// DGLog
+///
+/// - Parameters:
+///   - items: Zero or more items to print.
+///   - file: file name
+///   - function: function name
+///   - line: line number
+/// - Returns: log description
 @discardableResult
-public func DGLog(_ message: String = "",
-                  args: CVarArg ...,
-    function: String = #function,
-    file: String = #file,
-    line: Int = #line) -> String {
+public func DGLog(_ items: Any ..., file: String = #file, function: String = #function, line: Int = #line) -> String {
     
     #if DEBUG
+    typealias config = DGLogConfig
+    
+    var arr = [String]()
+    
+    let formatter: DateFormatter = {
+        let result = DateFormatter()
+        result.dateFormat = "yyyy/MM/dd HH:mm:ss.SSS"
+        return result
+    }()
+    
+    if config.isVisibleDate {
+        arr.append(formatter.string(from: Date()))
+    }
+    if config.isVisibleFile, config.isVisibleFunction {
+        arr.append("[\(URL(fileURLWithPath: file).lastPathComponent) \(function)]")
+    } else if config.isVisibleFile, !config.isVisibleFunction {
+        arr.append("[\(URL(fileURLWithPath: file).lastPathComponent)]")
+    } else if !config.isVisibleFile, config.isVisibleFunction {
+        arr.append("[\(function)]")
+    }
+    if config.isVisibleLine {
+        arr.append("(\(line))")
+    }
+    if config.isVisibleMessage {
+        arr.append(items.map({ String(describing: $0) }).joined(separator: " "))
+    }
+    if config.isVisible {
+        let message = arr.joined(separator: " ")
+        print(message)
+        return message
+    } else {
+        return ""
+    }
     #else
+    return ""
     #endif
-    
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-    let date = formatter.string(from: Date())
-    let fileName = URL(fileURLWithPath: file).lastPathComponent
-    let prefix = "\(date) [\(fileName) \(function)](\(line))"
-    let body = String(format: "\(message)", args)
-    let suffix = ""
-    let result = prefix + body + suffix
-    
-    print(result)
-    
-    return result
 }
