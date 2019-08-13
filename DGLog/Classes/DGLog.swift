7652c82604e6ba62bfde7aea744a60470e06642a
daeung Kim <dwkim@gpmobile.co.kr>
2019-08-13 16:55:03 +0900

    1. example (OSX) project setting modify 2. Podfile modify 3. log modify

--- DGLog/Classes/DGLog.swift
+++ DGLog/Classes/DGLog.swift
@@ -1,3 +1,35 @@
-public func DGLog() {
-    print("test")
+import Foundation
+
+public struct DGLogConfig {
+    public static var isVisible = true
+    public static var isVisibleDate = true
+    public static var isVisibleFile = true
+    public static var isVisibleFunction = true
+    public static var isVisibleLine = true
+    public static var isVisibleMessage = true
+}
+
+@discardableResult
+public func DGLog(_ message: String = "",
+                  args: CVarArg ...,
+    function: String = #function,
+    file: String = #file,
+    line: Int = #line) -> String {
+    
+    #if DEBUG
+    #else
+    #endif
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    let date = formatter.string(from: Date())
+    let fileName = URL(fileURLWithPath: file).lastPathComponent
+    let prefix = "\(date) [\(fileName) \(function)](\(line))"
+    let body = String(format: "\(message)", args)
+    let suffix = ""
+    let result = prefix + body + suffix
+    
+    print(result)
+    
+    return result
 }
